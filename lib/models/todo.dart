87eac3cb6334b8a8dc905343ad2bdaa96ede3204
kubracetinkaya1 <kubra.cetinkaya0@ogr.dpu.edu.tr>
2026-09-09 10:45:18 +0300

    get_storage ile localde tutulmaya başlandı

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,13 +1,23 @@
 class Todo {
   final String id;
   String text;
-  bool isDone;  
+  bool isDone;
 
   Todo({
     required this.id,
     required this.text,
     this.isDone = false,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'text': text,
+        'isDone': isDone,
+      };
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+        id: json['id'] as String,
+        text: json['text'] as String,
+        isDone: json['isDone'] as bool? ?? false,
+      );
 }
-
-
